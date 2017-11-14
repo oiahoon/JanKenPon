03cc61c3318fa79b6d61a327f4d087cb1594ad50
adong1 <annie.dong@activenetwork.com>
2017-11-14 10:37:04 +0800

    add validation for username&password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 class User < ApplicationRecord
   acts_as_authentic do |c|
+    c.login_field = :username
     c.validate_login_field = false
     c.crypto_provider = Authlogic::CryptoProviders::SCrypt
+    c.validates_length_of_password_field_options = { in: 4..20 }
   end
 
   has_many :punch
   has_one :user_score
 
+  validates :username, uniqueness: true, presence: true, length: { in: 4..40 }
 end
