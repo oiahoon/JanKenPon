ddb9e18512fc94e0e6723e8ebf78e8a6a2a60212
onice <4296411@qq.com>
2017-11-20 01:02:29 +0800

    temprary calculate rank

--- app/controllers/rank_controller.rb
+++ app/controllers/rank_controller.rb
@@ -1,5 +1,6 @@
 class RankController < ApplicationController
   def index
-
+    user_list = User.includes(:user_score).order("user_scores.total_score desc").limit(100)
+    render json: user_list
   end
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,9 +1,28 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :total_score
+  attributes :id, :username, :total_score, :last_punch_at, :win_rate
 
   has_one :user_score
+  cached
+  delegate :cache_key, to: :object
 
   def total_score
     object.user_score.total_score
+  end
+
+  def last_punch_at
+    punch = object.punch.last
+    punch.present? ? punch.created_at.to_s(:db) : nil
+  end
+
+  def win_rate
+    punch_list = Punch.joins("LEFT JOIN punch_records ON punches.punch_record_id = punch_records.winner_punch_id")\
+                      .where(user_id: object.id)
+                  # .where("punches.punch_record_id IS NOT NULL AND punches.punch_record_id > 0")\
+                  # .where("punch_records.winner_punch_id")
+    win_times = 0
+    punch_list.each do |rst|
+      win_times += 1 if rst.win?
+    end
+    sprintf '%.2f', (win_times.to_f / punch_list.count)
   end
 end
