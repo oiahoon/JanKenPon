68bb3ffac6db0556bb0835cb81a18c1bc9fb8b1b
onice <4296411@qq.com>
2017-11-21 23:52:10 +0800

    session key

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,9 @@
-Rails.application.config.session_store :redis_store, servers: "redis://redis:6379/0/session"
+Rails.application.config.session_store :redis_store, {
+  servers: "redis://redis:6379/0/session",
+  key:           '_jkp-session',
+  path:          '/',
+  domain:        nil,   # accepted domain, for example '.example.com'
+  expire_after:  nil,   # the session will be expired in X seconds unless active
+  secure:        false, # if true, cookie is valid only on https
+  httponly:      true   # if true, javascript can't access the cookie
+}
