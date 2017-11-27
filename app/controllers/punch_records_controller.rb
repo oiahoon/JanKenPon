23746d3a59ce6ed278eacfd9f8658ed99989019c
jhuang <Joey.Huang@activenetwork.com>
2017-11-27 18:48:48 +0800

    add history api

--- app/controllers/punch_records_controller.rb
+++ app/controllers/punch_records_controller.rb
@@ -1,3 +1,19 @@
 class PunchRecordsController < ApplicationController
 
+  PER_PAGE = 50
+
+  def index
+    records = PunchRecord.last(PER_PAGE)
+
+  end
+
+  format_record = Struct.new(:user_a, :user_b, :result) do
+    def to_object
+      {
+        "rand": (3 + rand(3)),
+        "text": "#{user_a}"
+      }
+    end
+  end
+
 end
